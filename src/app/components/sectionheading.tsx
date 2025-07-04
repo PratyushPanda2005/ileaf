ba40c4e3bb074f6a2ca93968de8f03d8e227ca26
Pratyush005 <pandapratyush005@gmail.com>
2025-07-04 11:22:35 +0530

    Hero Page Fixes

--- src/app/components/sectionheading.tsx
+++ src/app/components/sectionheading.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Parallelogram from './parallelogram'
 
 const SectionHeading = () => {
   return (
-    <div className='flex md:hidden flex-col gap-6 justify-center items-center p-10 bg-[#FFBF00] md:'>
-        <h1 className='text-xl font-blafhyglibs italic text-black tracking-[4px]'>Products Range</h1>
-        <div className='hidden lg:flex w-4 h-2 bg-amber-300 transform skew-x-[-200deg]'/>
+    <div className='flex flex-col justify-center items-center p-10 md:p-20 md:bg-[#707070]'>
+        <h1 className='text-xl md:text-2xl lg:text-3xl font-blafhyglibs italic text-black md:text-white tracking-[4px]'>Products Range</h1>
+        <Parallelogram/>
     </div>
   )
 }
