dd748fd1d4289e0d26bfef33f206091969c88392
Pratyush005 <pandapratyush005@gmail.com>
2025-07-06 21:03:55 +0530

    MObile Version

--- src/app/product-category/luxury/[subcategory]/page.tsx
+++ src/app/product-category/luxury/[subcategory]/page.tsx
@@ -22,22 +22,21 @@ async function DoorDetailsPage({ params } : {params : Params}) {
   return (
     <section className="min-h-screen bg-black">
       <ProductCategory/>
-     <div className=" grid grid-cols-3 px-20">
-        <div className="col-span-2 bg-[#5a5a5a] p-20">
-            <div className="flex h-full w-full">
+     <div className=" grid lg:grid-cols-3 px-10 lg:px-20">
+        <div className="lg:col-span-2 bg-[#5a5a5a] p-10 lg:p-20">
+            <div className="flex h-full w-full gap-10">
             <div className="h-full w-full flex flex-col justify-center items-center">
-            <h1 className="font-style text-3xl mb-10">{doorDetails.id}</h1>
-            <Image src={doorDetails.image} width={300} height={300} alt="" />
+            <h1 className=" text-[#FFBF00] lg:text-black font-style text-xl md:text-2xl lg:text-3xl mb-10">{doorDetails.id}</h1>
+            <Image src={doorDetails.image} width={300} height={300} alt="" className="max-sm:h-[240px] max-sm:w-auto" />
             </div>
-            <div className="h-full w-full flex items-center justify-center">
-            <p className="font-raleway tracking-[1px] text-[14px] font-[300] leading-[180%] max-w-[280px]">{description.description}</p>
+            <div className="h-full w-full items-center justify-center hidden lg:flex">
+            <p className="font-raleway tracking-[1px] text-[14px] font-[300] leading-[180%] max-w-[280px] ">{description.description}</p>
             </div>
             </div>
-
         </div>
-        <div className="col-span-1 bg-[#6b6b6b] text-[#ffc600] font-raleway font-[300] leading-[180%] flex justify-center items-center p-20 tracking-[1px] text-[14px]">
-            <ul>
-            <li className="leading-[180%]"><span className="font-semibold ">Door leaf thickness: </span>{doorDetails.details.doorLeafThickness} </li>
+        <div className="col-span-1 bg-[#DBDBDB] lg:bg-[#6b6b6b] text-black lg:text-[#ffc600] font-raleway font-[300] leading-[180%] flex justify-center items-center p-10 xl:p-20 tracking-[1px] text-[14px]">
+            <ul className=" text-center lg:text-left">
+            <li className="leading-[180%]"><span className="font-semibold">Door leaf thickness: </span>{doorDetails.details.doorLeafThickness} </li>
             <li>{doorDetails.details.specialframes}</li>
             <li><span className="font-semibold">Structured door frame sheet thickness: </span>{doorDetails.details.frameSheetThickness} </li>
             <li>{doorDetails.details.panelStuffing}</li>
