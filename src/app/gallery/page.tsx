3e6fa0d56a4cd6fcc27915bd93b3026978a0523a
Pratyush005 <pandapratyush005@gmail.com>
2025-07-08 03:19:14 +0530

    Gallery Page Added

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,8 +1,54 @@
 import React from 'react'
-
+import Cycling from '@/../public/maico-amorim-SJWPKMb9u-k-unsplash.jpg'
+import Image from 'next/image'
 const Gallery = () => {
+  const list =[
+    {
+      id: 0
+    },
+    {
+      id: 1
+    },
+    {
+      id: 2
+    },
+    {
+      id: 3
+    },
+    {
+      id: 4
+    },
+    {
+      id: 5
+    }
+  ]
+  const marginClasses = [
+    'translate-x-[0px]', 
+    'translate-x-[0px]',         
+    '-translate-x-[34.333px]', 
+    '-translate-x-[34.333px]',           
+    '-translate-x-[68.666px]', 
+    '-translate-x-[68.666px]'          
+  ];
+  
+  
   return (
-    <div>Gallery</div>
+    <section className='min-h-screen flex justify-center items-center py-[120px] bg-black px-20 lg:px-20'>
+      <div className='flex flex-col gap-10 lg:flex-row w-full justify-between'>
+      <ul className='text-[#707070] font-style !tracking-[1px] space-y-4 text-sm text-center lg:text-left'>
+          <li>Video Gallery</li>
+          <li>Photo Gallery</li>
+          <li>Download Brochure</li>
+      </ul>
+      <div className='grid lg:grid-cols-2 gap-10'>
+      {list.map((item) => (
+          <div key={item.id} className='col-span-1 -skew-x-6'>
+            <Image src={Cycling} alt='Cycling' className={`w-96 h-64 ${marginClasses[item.id]}`} />
+          </div>
+      ))}
+      </div>
+      </div>
+    </section>
   )
 }
 
