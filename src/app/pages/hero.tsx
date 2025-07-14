df58626d7b0a7b1fdd97dc1e90bfe029ab6b258d
Pratyush005 <pandapratyush005@gmail.com>
2025-07-14 23:15:29 +0530

    C

--- src/app/pages/hero.tsx
+++ src/app/pages/hero.tsx
@@ -29,10 +29,10 @@ const Hero = () => {
     <div className="h-[calc(60vh-64px)] sm:min-h-screen relative top-0 left-0 w-full">
       <div className="relative h-[60vh] sm:h-screen w-full">
         <div ref={bgRef}
-          className="absolute inset-0 bg-cover bg-no-repeat animate-bg-mob-bounce sm:animate-bg-bounce will-change: transform"
+          className="absolute inset-0 bg-cover bg-center bg-no-repeat animate-bg-mob-bounce will-change: transform"
           style={{
             backgroundImage: 'url("https://res.cloudinary.com/db4zbyipc/image/upload/v1752411438/home-hero_1_1_zlk7mp.webp")',
-            backgroundPosition: '0% 30%', 
+            backgroundPosition: 'center 30%', 
             willChange: 'background-position'
             
           }}
